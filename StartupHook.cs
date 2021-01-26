c4714dd18b690a8a82588afb768d9e0dedcb54ce
hans-peter.weidinger <hans-peter.weidinger@dynatrace.com>
2021-01-26 17:21:54 +0100

    Make createdump executeable configurable

--- StartupHook.cs
+++ StartupHook.cs
@@ -3,17 +3,20 @@ using System.Diagnostics;
 using System.Runtime.ExceptionServices;
 
 internal class StartupHook {
-    private static String DumpDir = "/tmp";
+    private static string DumpDir = "/tmp";
+    private static string DumpExecutable { get; set; }
     private static int DumpNumber = 0;
     
     public static void Initialize() {
         DumpDir = Environment.GetEnvironmentVariable("DT_CRASH_DUMP_DIR") ?? DumpDir;
+        DumpExecutable = Environment.GetEnvironmentVariable("DT_DUMP_EXEC") ?? "createdump";
         
         Console.Out.WriteLine($"Exception Grabber active - wrtiting dumps to {DumpDir}");
         
         AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
         AppDomain.CurrentDomain.FirstChanceException +=CurrentDomainOnFirstChanceException;
     }
+
 
     private static void CurrentDomainOnFirstChanceException(object? sender, FirstChanceExceptionEventArgs e) {
         if (e.Exception is System.IO.IOException && (e.Exception?.Message?.StartsWith("The process cannot access the file") ?? false)) {
@@ -27,7 +30,7 @@ internal class StartupHook {
 
     private static void CreateDump(string name, Exception ex) {
         try {
-            Process.Start("createdump", $"--full --name {DumpDir}/{name}-{((ex != null) ? ex.GetType().Name : "null")}-%p-%t({DumpNumber++}).core {Process.GetCurrentProcess().Id}")?.WaitForExit(30_000);
+            Process.Start(DumpExecutable, $"--full --name {DumpDir}/{name}-{((ex != null) ? ex.GetType().Name : "null")}-%p-%t({DumpNumber++}).core {Process.GetCurrentProcess().Id}")?.WaitForExit(30_000);
         } 
         catch (Exception e) {
             Console.Out.WriteLine($"Failed to get dump number {DumpNumber}. Reason: {e}");
